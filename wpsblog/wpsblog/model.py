17b084e154d97630555ec8493699019725eb1c1e
feanar729 <feanar729@gmail.com>
2016-06-06 18:22:22 +0900

    deleted .swp file, refactor models.py

--- wpsblog/wpsblog/model.py
+++ wpsblog/wpsblog/model.py
@@ -1,10 +1,11 @@
 from django.db import models
 
+
 class Post(models.Model):
     title = models.CharField(
         mas_length=120,
     )
-	content = models.TextField()
-    
-	def __str__(self):
+    content = models.TextField()
+
+    def __str__(self):
         return self.title
